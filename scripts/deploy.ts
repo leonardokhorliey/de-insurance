fa3f5ccc9e60b68319075dc27174343f5578130f
leonardokhorliey <ebubeokoli@rocketmail.com>
2022-12-21 22:27:53 +0100

    Update scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -19,66 +19,66 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  // console.log("Hi")
-  // const dataForCode = await uploadImage('code.png', true, 'Comprehensive Vehicle Insurance', 'Insure your vehicle with a full comprehensive insurance', '9.00');
-  // console.log("Yo")
-  // const dataForPic = await uploadImage('pic.png', false, '3rd-Party Vehicle Insurance', 'Save the time for the delay. Let\'s cover you when you make mistakes.', '15.28');
+  console.log("Hi")
+  const dataForCode = await uploadImage('code.png', true, 'Comprehensive Vehicle Insurance', 'Insure your vehicle with a full comprehensive insurance', '9.00');
+  console.log("Yo")
+  const dataForPic = await uploadImage('pic.png', false, '3rd-Party Vehicle Insurance', 'Save the time for the delay. Let\'s cover you when you make mistakes.', '15.28');
 
-  // const jsonData1 = await uploadToIpfs([
-  //   {
-  //     path: `2.json`,
-  //     content: dataForCode
-  //   },
-  // ], false);
+  const jsonData1 = await uploadToIpfs([
+    {
+      path: `2.json`,
+      content: dataForCode
+    },
+  ], false);
 
-  // const jsonData2 = await uploadToIpfs([
-  //   {
-  //     path: `3.json`,
-  //     content: dataForPic
-  //   }
-  // ], false)
+  const jsonData2 = await uploadToIpfs([
+    {
+      path: `3.json`,
+      content: dataForPic
+    }
+  ], false)
 
-  // const ipfsUrl = jsonData1[0].path.replace('/2.json', '');
+  const ipfsUrl = jsonData1[0].path.replace('/2.json', '');
 
-  // console.log(ipfsUrl);
-
-
-  // const MockUSDT = await ethers.getContractFactory("ERC20Mock");
-  // const mockUSDT = await MockUSDT.deploy();
-
-  // await mockUSDT.deployed();
-
-  // console.log("Token deployed to:", mockUSDT.address);
-
-  // const Token = await ethers.getContractFactory("DeInsureToken");
-  // const token = await Token.deploy();
-
-  // await token.deployed();
-
-  // await token.createNewPackage('900', jsonData1[0].path);
-  // await token.createNewPackage('1528', jsonData2[0].path);
-
-  // console.log("Token deployed to:", token.address);
+  console.log(ipfsUrl);
 
 
+  const MockUSDT = await ethers.getContractFactory("ERC20Mock");
+  const mockUSDT = await MockUSDT.deploy();
 
-  const alchemyProvider = new ethers.providers.AlchemyProvider("maticmum", process.env.ALCHEMY_API_KEY);
+  await mockUSDT.deployed();
 
-// Signer
-  const signer = new ethers.Wallet(process.env.PRIVATE_KEY || '', alchemyProvider);
+  console.log("Token deployed to:", mockUSDT.address);
 
-// Contract
-  const token = new ethers.Contract("0xAE657Be47195f0a2AC3b13c3fd1BA30A3C6234Bd", tokenJson.abi, signer);
+  const Token = await ethers.getContractFactory("DeInsureToken");
+  const token = await Token.deploy();
+
+  await token.deployed();
+
+  await token.createNewPackage('900', jsonData1[0].path);
+  await token.createNewPackage('1528', jsonData2[0].path);
+
+  console.log("Token deployed to:", token.address);
+
+
+
+//   const alchemyProvider = new ethers.providers.AlchemyProvider("maticmum", process.env.ALCHEMY_API_KEY);
+
+// // Signer
+//   const signer = new ethers.Wallet(process.env.PRIVATE_KEY || '', alchemyProvider);
+
+// // Contract
+//   const token = new ethers.Contract("0xAE657Be47195f0a2AC3b13c3fd1BA30A3C6234Bd", tokenJson.abi, signer);
 
   const Verifier = await ethers.getContractFactory("Verifier");
-  const verifier = await Verifier.deploy("0x5F92394708b560383B19a69bC7dC75549EC15B41");
+  const verifier = await Verifier.deploy(mockUSDT.address);
 
   await verifier.deployed();
 
   console.log("Verifier deployed to:", verifier.address);
 
   const Pool = await ethers.getContractFactory("InsurancePool");
-  const pool = await Pool.deploy("0x5F92394708b560383B19a69bC7dC75549EC15B41", verifier.address, "0xAE657Be47195f0a2AC3b13c3fd1BA30A3C6234Bd");
+  const pool = await Pool.deploy(mockUSDT.address, verifier.address, token.address);
 
   await pool.deployed();
 
